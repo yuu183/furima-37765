9902ecd258be1bae04234c36b99dc619e271faa1
yuu183 <y.toyama888@gmail.com>
2022-08-18 12:53:26 +0900

    ユーザー管理修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up,keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
   end
 end
