0ea8d226fbf491262a4654d29403b09b7cded0fb
yuu183 <y.toyama888@gmail.com>
2022-08-18 15:31:29 +0900

    バリデーション記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price
+    validates :user_id
+  end
+
 end
