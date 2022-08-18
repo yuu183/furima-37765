8b82e9bfa01c09900a1fdc8ab01f59debd177222
yuu183 <y.toyama888@gmail.com>
2022-08-18 16:56:51 +0900

    ActiveHashバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,9 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :genre_id, numericality: { other_than: 1 } 
+
   belongs_to :user
   has_one_attached :image
 
@@ -19,6 +22,14 @@ class Item < ApplicationRecord
     validates :shipping_day_id
     validates :price
     validates :user_id
+  end
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"}  do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :shipping_day_id
   end
 
 end
