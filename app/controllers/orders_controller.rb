c3ef8cb5af603b56e706a874041fe49883416bfc
yuu183 <y.toyama888@gmail.com>
2022-08-19 09:44:14 +0900

    order_controller編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
   def create
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       return redirect_to root_path
     else
@@ -18,6 +19,15 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_form).permit(:post_code, :prefecture_id, :city, :adress, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 end
